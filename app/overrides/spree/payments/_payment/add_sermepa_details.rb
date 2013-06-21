9dd43c9dfaba2597839ca1228392d3a7c7c01d67
Eduardo Moreno <hmitsurugi@gmail.com>
2013-06-21 11:42:30 +0200

    Added :original tag to Deface 'add_sermepa_details'.

--- app/overrides/spree/payments/_payment/add_sermepa_details.rb
+++ app/overrides/spree/payments/_payment/add_sermepa_details.rb
@@ -1,6 +1,7 @@
 Deface::Override.new(:virtual_path => "spree/payments/_payment", 
                      :name => "add_sermepa_details", 
-                     :insert_before => "code[erb-silent]:contains('else')") do
+                     :insert_before => "code[erb-silent]:contains('else')",
+                     :original => 'de4a56e1f34dbad82c32ae1bb851721210e221cc') do
   '<% elsif payment.source_type.to_s.include?("Sermepa") %> 
   <span class="cc-type">
   <%= image_tag "credit_cards/credit_card.gif", :id => "creditcard-sermepa" %>
